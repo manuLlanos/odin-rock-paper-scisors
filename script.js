70287db74c3def99e6ec539769c9d2f2f2274339
manuLlanos <26463463+manuLlanos@users.noreply.github.com>
2024-09-10 19:48:32 -0300

    Add getComputerChoice and getRandomInteger functions

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 "use strict";
 
-console.log("Hello")
+
+// returns an integer between 0 and n-1
+function getRandomInteger(n) {
+    return Math.floor((Math.random() * n));
+}
+
+// Returns "rock" "paper" or "scissors" randomly
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[getRandomInteger(3)];
+}
